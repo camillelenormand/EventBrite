fdfe7388bd978d4ea225bd767bbb5e561b81a45f
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-15 17:34:10 +0200

    remove sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,15 +77,5 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :mailjet
-
-  ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_LOGIN'],
-    :password => ENV['SENDGRID_PWD'],
-    :domain => 'monsite.fr',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
   
 end
